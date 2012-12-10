937ee6c3ad40cffc01ce1efd9b27dc9f95d08720
Max Batt <mail@maxbatt.de>
2012-12-10 22:33:30 +0100

    ErrorHandler kommentiert

--- src/hdm/stuttgart/esell/errors/ErrorHandler.java
+++ src/hdm/stuttgart/esell/errors/ErrorHandler.java
@@ -4,20 +4,38 @@ import java.util.HashMap;
 
 import com.google.gson.Gson;
 
+/*
+ * ErrorHandler
+ * Führt einen Katalog mit Fehler- und Erfolgsmeldungen und stellt ein Objekt mit den Feldern flag und message.
+ * Je nach errorCode, mit dem instanziiert wird, wird flag auf true/false gesetzt und message mit 
+ * der entsprechenden Fehlermeldung belegt. 
+ * 
+ */
+
 public class ErrorHandler {
 
 	private boolean flag;
 	private String message;
-
+	
+	/*
+	 * Konstruktor
+	 * Params: int flag, String errorCode
+	 * 
+	 */
 	public ErrorHandler(boolean flag, String errorCode) {
 		this.flag = flag;
 		
+		//HashMap für Meldungen erzeugen
 		HashMap<String, String> errorMap = new HashMap<String, String>();
+		//HashMap mit Einträgen füllen
 		errorMap = fillErrorMap(errorMap);
 		
+		//überprüfen, ob der übergebene Fehlercode in der HashMap liegt.
+		//Wenn ja, entsprechende Meldung ausgeben
 		if (errorMap.containsKey(errorCode)) {
 			this.message = errorMap.get(errorCode).toString();
 		} 
+		//Wenn nein, Standardfehlermeldung
 		else {
 			this.message = "Fehler";
 		}
@@ -25,24 +43,23 @@ public class ErrorHandler {
 
 
 	
-
+	//KAtalog für Fehler- und Erfolgsmeldungen
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private HashMap<String, String> fillErrorMap(HashMap errorMap) {
-		errorMap.put("INSERT_ERR", "Fehler beim Speichern in der Datenbank");
-		errorMap.put("UPDATE_ERR", "Fehler beim Aktualisieren");
-		errorMap.put("DELETE_ERR", "Fehler beim Löschen");
-		errorMap.put("USR_EXISTS", "Username oder Email schon vorhanden");
 		errorMap.put("DB_ERR", "Fehler beim Verbinden zur Datenbank");
-		errorMap.put("USR_SAVED", "Dein Account wurde erfolgreich angelegt");
-		errorMap.put("USR_UPDATED", "Dein Profil wurde aktualisiert");
-		errorMap.put("USR_DELETED", "Der Benutzer wurde erfolgreich gelöscht");
+		errorMap.put("DELETE_ERR", "Fehler beim Löschen");
+		errorMap.put("INSERT_ERR", "Fehler beim Speichern in der Datenbank");
+		errorMap.put("NO_ENTRY_ERR", "Kein DB-Eintrag vorhanden");
 		errorMap.put("PET_SAVED", "Dein Kaufgesuch wurde gespeichert");
 		errorMap.put("PET_UPDATED", "Dein Kaufgesuch wurde aktualisiert");
 		errorMap.put("PET_DELETED", "Das Kaufgesuch wurde gelöscht");
-		errorMap.put("NO_ENTRY_ERR", "Kein DB-Eintrag vorhanden");
+		errorMap.put("UPDATE_ERR", "Fehler beim Aktualisieren");
+		errorMap.put("USR_EXISTS", "Username oder Email schon vorhanden");
+		errorMap.put("USR_SAVED", "Dein Account wurde erfolgreich angelegt");
+		errorMap.put("USR_UPDATED", "Dein Profil wurde aktualisiert");
+		errorMap.put("USR_DELETED", "Der Benutzer wurde erfolgreich gelöscht");
 		errorMap.put("USR_HAS_PET_ERR", "Der Benutzer kann nicht gelöscht werden, weil ihm noch Kaufgesuche zugewiesen sind");
 
-		
 		return errorMap;
 	}
 	
@@ -55,6 +72,8 @@ public class ErrorHandler {
 	}
 	
 
+	
+	//Getter und Setter
 	public boolean getFlag() {
 		return this.flag;
 	}
