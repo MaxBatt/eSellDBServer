4ae5fd7186ea97e1a6eeef072c3367121cffe569
ScratchFish <scratchfish@googlemail.com>
2012-12-13 18:07:15 +0100

    getPetitionListByCategory
    Zusätzlich noch kleinere Änderungen an dem SQL-Skripten in der
    PetitionList Klasse.

--- src/hdm/stuttgart/esell/Model/PetitionList.java
+++ src/hdm/stuttgart/esell/Model/PetitionList.java
@@ -32,8 +32,12 @@ public class PetitionList extends Persistence{
         {
             try {
             	//Statement vorbereiten
-                String sql = "SELECT * from petitions ORDER BY " + order + " LIMIT " + start + ", " + limit;
+                String sql = "SELECT id from petitions ORDER BY ? LIMIT ?, ?";
                 preparedStatement = conn.prepareStatement(sql);
+                
+                preparedStatement.setString(1, order);
+                preparedStatement.setInt(2, start);
+                preparedStatement.setInt(3, limit);
                 
                 //Statement absetzen
                 ResultSet result = preparedStatement.executeQuery();
@@ -71,8 +75,13 @@ public class PetitionList extends Persistence{
         {
             try {
             	//Statement vorbereiten
-                String sql = "SELECT * from petitions WHERE user_id = " + userID + " ORDER BY " + order + " LIMIT " + start + ", " + limit;
+                String sql = "SELECT id from petitions WHERE user_id = ? ORDER BY ? LIMIT ?, ?";
                 preparedStatement = conn.prepareStatement(sql);
+                
+                preparedStatement.setInt(1, userID);
+                preparedStatement.setString(2, order);
+                preparedStatement.setInt(3, start);
+                preparedStatement.setInt(4, limit);
                 
                 //Statement absetzen
                 ResultSet result = preparedStatement.executeQuery();
@@ -96,7 +105,46 @@ public class PetitionList extends Persistence{
         	throw new ErrorHandler(ErrorHandler.ErrorCode.DB_ERR);
 	}
 	
-	
+	//Gibt die PetitionList einer bestimmten Kategorie zurück
+		//Empfängt als Parameter CategoryID,  Sortierung, Start-Zeile und Limit
+		public static PetitionList getPetitionListByCategory(int categoryID, String order, int start, int limit) throws ErrorHandler{
+			
+			makeConnection();
+	    	PreparedStatement preparedStatement = null;
+	    	
+	        if(conn != null)
+	        {
+	            try {
+	            	//Statement vorbereiten
+	                String sql = "SELECT id from petitions WHERE category_id = ? ORDER BY ? LIMIT ?, ?";
+	                preparedStatement = conn.prepareStatement(sql);
+	                
+	                preparedStatement.setInt(1, categoryID);
+	                preparedStatement.setString(2, order);
+	                preparedStatement.setInt(3, start);
+	                preparedStatement.setInt(4, limit);
+	                
+	                //Statement absetzen
+	                ResultSet result = preparedStatement.executeQuery();
+	                
+	                ArrayList<Petition> petitionList = new ArrayList<Petition>();
+	                
+	                //Für jeden Datensatz ein Objekt anlegen und in die Liste packen
+	                while(result.next())
+	                {
+	                	Petition petition = Petition.getPetition(result.getInt("id"));
+	                	petitionList.add(petition);
+	                }
+	                
+	                return new PetitionList(petitionList);
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	                throw new ErrorHandler(ErrorHandler.ErrorCode.DB_ERR);
+	            }
+	        }
+	        else
+	        	throw new ErrorHandler(ErrorHandler.ErrorCode.DB_ERR);
+		}
 	
 	
 	//Json ausgeben
